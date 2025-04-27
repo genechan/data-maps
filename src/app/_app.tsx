c8b95175cefd0458b7f4d6117914f8059ebbc618
genechan <667922+genechan@users.noreply.github.com>
2025-04-27 13:24:15 -0400

    update to use material ui

---
+++ src/app/_app.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+
+// Optionally create a custom theme
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#1976d2",
+    },
+    secondary: {
+      main: "#dc004e",
+    },
+  },
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
